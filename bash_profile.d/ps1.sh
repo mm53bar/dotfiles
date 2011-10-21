0581b51fa9c1af375f67edee1ea135d94faa2e73
Michael McClenaghan <mike@sideline.ca>
2011-10-21 11:54:08 -0600

    Updated prompt to use rbenv version

--- bash_profile.d/ps1.sh
+++ bash_profile.d/ps1.sh
@@ -9,6 +9,15 @@ function parse_git_dirty {
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ [\1$(parse_git_dirty)]/"
 }
-
+function rbenv_ps1() {
+  if [ -d ~/.rbenv ]; then
+    local rbenv_ps1="$(rbenv version-name)"
+    if [ -n "${rbenv_ps1}" ]; then
+      if [ "system" != "${rbenv_ps1}" ]; then
+        printf "${1:-%s}" "$rbenv_ps1"
+      fi
+    fi
+  fi
+}
 # export PS1='\u@\h:\[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
-export PS1='$(~/.rvm/bin/rvm-prompt):\[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
+export PS1='$(rbenv_ps1):\[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
