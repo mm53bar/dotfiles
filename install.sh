e19207a33aad36986acb77bc0e6281581af51c38
Michael McClenaghan <mike@sideline.ca>
2012-09-12 12:28:10 -0600

    Now contains a default global config for Bundler gem.

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ ln -fs ~/.dotfiles/inputrc ~/.inputrc
 ln -fs ~/.dotfiles/irbrc ~/.irbrc
 ln -fs ~/.dotfiles/rvmrc ~/.rvmrc
 
+mkdir -p ~/.bundle
+ln -fs ~/.dotfiles/bundle_config ~/.bundle/config
+
 [ ! -h ~/.bash_profile.d ] && ln -fs ~/.dotfiles/bash_profile.d/ ~/.bash_profile.d
 [ ! -e ~/.bash_profile.d/private/ ] && mkdir ~/.bash_profile.d/private/
 
