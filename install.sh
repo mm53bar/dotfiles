4a4d8f86adfb39c2fdbaa9b98641c13179c4f98d
Mike McClenaghan <mike@sideline.ca>
2013-01-19 13:07:55 -0700

    Added gitconfig.local to stop people from using my creds in git commits

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+echo "Installing dotfiles"
+
 ln -fs ~/.dotfiles/autotest ~/.autotest
 ln -fs ~/.dotfiles/bashrc ~/.bashrc
 ln -fs ~/.dotfiles/bash_profile ~/.bash_profile
@@ -10,10 +12,21 @@ ln -fs ~/.dotfiles/rvmrc ~/.rvmrc
 ln -fs ~/.dotfiles/gitignore ~/.gitignore
 ln -fs ~/.dotfiles/ruby-version ~/.ruby-version
 
+echo "Installing .bundle/config"
+
 mkdir -p ~/.bundle
 ln -fs ~/.dotfiles/bundle_config ~/.bundle/config
 
-[ ! -h ~/.bash_profile.d ] && ln -fs ~/.dotfiles/bash_profile.d/ ~/.bash_profile.d
-[ ! -e ~/.bash_profile.d/private/ ] && mkdir ~/.bash_profile.d/private/
+echo "Installing .bash_profile.d"
 
+[ ! -h ~/.bash_profile.d ] && ln -fs ~/.dotfiles/bash_profile.d/ ~/.bash_profile.d
+
+echo "Initializing .bash_profile.d/private and .gitconfig.local"
+
+[ ! -e ~/.bash_profile.d/private/ ] && mkdir ~/.bash_profile.d/private/
+[ ! -f ~/.gitconfig.local ] && cp ~/.dotfiles/gitconfig.local.example ~/.gitconfig.local
+
+echo ""
+
+echo "Good work. We're ready to rock. Be sure to update ~/.gitconfig.local"
 
