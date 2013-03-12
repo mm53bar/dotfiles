e4ee1e5d9b78f7bd0b015e19474f1ad45c5df7fe
Mike McClenaghan <mike@sideline.ca>
2013-03-11 19:41:36 -0600

    Added default ec2 endpoint url

--- bash_profile.d/ec2_api_tools.sh
+++ bash_profile.d/ec2_api_tools.sh
@@ -1,3 +1,4 @@
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+export EC2_URL=https://ec2.us-east-1.amazonaws.com
 export PATH=$PATH:$EC2_HOME/bin 
