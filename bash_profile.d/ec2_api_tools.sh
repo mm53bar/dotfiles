402ffeabd812d2979f17acd84eff9cc15f25abc9
Mike McClenaghan <mike@sideline.ca>
2013-03-17 15:18:18 -0600

    Replaced relative reference with absolute

--- bash_profile.d/ec2_api_tools.sh
+++ bash_profile.d/ec2_api_tools.sh
@@ -2,5 +2,5 @@ export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
 export EC2_URL=https://ec2.us-east-1.amazonaws.com
 export PATH=$PATH:$EC2_HOME/bin 
-export AWS_ACCESS_KEY=$(cat private/AWS_ACCESS_KEY)
-export AWS_SECRET_KEY=$(cat private/AWS_SECRET_KEY)
+export AWS_ACCESS_KEY=$(cat $HOME/.bash_profile.d/private/AWS_ACCESS_KEY)
+export AWS_SECRET_KEY=$(cat $HOME/.bash_profile.d/private/AWS_SECRET_KEY)
