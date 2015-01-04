727d26752a1d0a842966df7a82c4469a5945fab7
Mike McClenaghan <mike@sideline.ca>
2015-01-03 17:12:54 -0700

    Don't need ec2_api_tools anymore

--- bash_profile.d/ec2_api_tools.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
-export EC2_URL=https://ec2.us-east-1.amazonaws.com
-export PATH=$PATH:$EC2_HOME/bin 
